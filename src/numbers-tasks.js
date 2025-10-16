7d186f6fe9197e10ee3cd15fac975d23aee27e9e
Evgeny Kuptsov <kupzov200@mail.ru>
2025-10-16 04:09:13 +0400

    feat: complete the isPrime task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -198,8 +198,12 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  let prime = true;
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) prime = false;
+  }
+  return prime;
 }
 
 /**
